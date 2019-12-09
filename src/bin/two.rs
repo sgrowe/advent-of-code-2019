12f90ea1df017b261bfb5abbfae0296661625151
Sam Rowe <sam.rowe@red-badger.com>
2019-12-09 14:04:39 +0000

    Complete day two part two

--- src/bin/two.rs
+++ src/bin/two.rs
@@ -3,16 +3,40 @@ mod file;
 fn main() {
   let input = file::read("src/two.txt");
 
+  println!("Part one:");
+  println!("Final value at position 0: {}", part_one(&input));
+
+  println!("Part two:");
+  let (noun, verb) = part_two(&input);
+  println!("Noun: {} - Verb: {}", noun, verb);
+  println!("100 * noun + verb: {}", (100 * noun) + verb);
+}
+
+fn part_one(input: &str) -> i64 {
   let mut program = parse_program(&input.trim());
 
-  // Restore program to the state it had just before the last computer caught fire
-  program[1] = 12;
-  program[2] = 2;
+  run_program_with_inputs(12, 2, &mut program);
 
-  run_program(&mut program);
+  program[0]
+}
 
-  println!("Part one:");
-  println!("Final value at position 0: {}", program[0]);
+fn part_two(input: &str) -> (i64, i64) {
+  let target = 19690720;
+
+  let initial_program = parse_program(&input.trim());
+
+  for i in 0..99 {
+    for j in 0..99 {
+      let mut program = initial_program.clone();
+      run_program_with_inputs(i, j, &mut program);
+
+      if program[0] == target {
+        return (i, j);
+      }
+    }
+  }
+
+  panic!("Could not find a pair of inputs resulting in {}", target);
 }
 
 fn parse_program(input: &str) -> Vec<i64> {
@@ -20,6 +44,13 @@ fn parse_program(input: &str) -> Vec<i64> {
     .split(',')
     .map(|s| s.parse::<i64>().unwrap())
     .collect::<Vec<i64>>()
+}
+
+fn run_program_with_inputs(a: i64, b: i64, program: &mut Vec<i64>) {
+  program[1] = a;
+  program[2] = b;
+
+  run_program(program)
 }
 
 fn run_program(program: &mut Vec<i64>) {
