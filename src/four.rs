1920edfab0bbb27360f2d953e695fba9f69c5ea2
Sam Rowe <sam.rowe@red-badger.com>
2019-12-11 15:04:53 +0000

    Complete day four part two

--- src/four.rs
+++ src/four.rs
@@ -1,25 +1,28 @@
 use std::ops::Range;
 
 pub fn main() {
+    let range = 109165..576724;
     println!("Part one:");
     println!(
         "Number of possible passwords: {}",
-        count_possible_pass_codes_in_range(109165..576724, &is_possible_pass_code)
+        count_possible_pass_codes_in_range(&range, &is_possible_pass_code)
     );
 
     println!();
     println!("Part two:");
     println!(
         "Number of possible passwords: {}",
-        count_possible_pass_codes_in_range(109165..576724, &is_possible_pass_code_v2)
+        count_possible_pass_codes_in_range(&range, &is_possible_pass_code_v2)
     );
 }
 
 fn count_possible_pass_codes_in_range(
-    range: Range<u32>,
+    range: &Range<u32>,
     predicate: impl Fn(&u32) -> bool,
 ) -> usize {
-    range.filter(&predicate).count()
+    let r = range.clone();
+
+    r.filter(&predicate).count()
 }
 
 fn is_possible_pass_code(code: &u32) -> bool {
@@ -31,8 +34,9 @@ fn is_possible_pass_code(code: &u32) -> bool {
 fn is_possible_pass_code_v2(code: &u32) -> bool {
     let digits = parse_digits(code);
 
-    // TODO:
-    is_six_digit(&digits) && has_adjacent_digits(&digits) && digits_only_increase(&digits)
+    is_six_digit(&digits)
+        && contains_an_adjacent_pair_of_digits(&digits)
+        && digits_only_increase(&digits)
 }
 
 fn parse_digits(code: &u32) -> Vec<u32> {
@@ -57,7 +61,7 @@ fn has_adjacent_digits(digits: &Vec<u32>) -> bool {
     };
 
     for digit in digits_iter {
-        if (digit) == prev_digit {
+        if digit == prev_digit {
             return true;
         };
 
@@ -65,6 +69,33 @@ fn has_adjacent_digits(digits: &Vec<u32>) -> bool {
     }
 
     false
+}
+
+fn contains_an_adjacent_pair_of_digits(digits: &Vec<u32>) -> bool {
+    let mut digits_iter = digits.iter();
+
+    let mut prev_digit = match digits_iter.next() {
+        Some(digit) => digit,
+        None => return false,
+    };
+
+    let mut adjacent_count = 1;
+
+    for digit in digits_iter {
+        if digit == prev_digit {
+            adjacent_count += 1;
+        } else {
+            if adjacent_count == 2 {
+                return true;
+            }
+
+            adjacent_count = 1;
+        }
+
+        prev_digit = digit;
+    }
+
+    adjacent_count == 2
 }
 
 fn digits_only_increase(digits: &Vec<u32>) -> bool {
@@ -100,5 +131,25 @@ mod part_one_tests {
     #[test]
     fn example_case_3() {
         assert!(!is_possible_pass_code(&123789));
+    }
+}
+
+#[cfg(test)]
+mod part_two_tests {
+    use super::*;
+
+    #[test]
+    fn example_case_1() {
+        assert!(is_possible_pass_code_v2(&112233));
+    }
+
+    #[test]
+    fn example_case_2() {
+        assert!(!is_possible_pass_code_v2(&123444));
+    }
+
+    #[test]
+    fn example_case_3() {
+        assert!(is_possible_pass_code_v2(&111122));
     }
 }
