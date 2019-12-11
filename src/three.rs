aebd886748180dd901aa0506d7cd51ef3cc521d4
Sam Rowe <sam.rowe@red-badger.com>
2019-12-11 14:46:19 +0000

    Complete day three part two

--- src/three.rs
+++ src/three.rs
@@ -6,6 +6,13 @@ pub fn main() {
 
     println!("Part one:");
     println!("Distance to closest crossover: {}", part_one(&input));
+
+    println!();
+    println!("Part two:");
+    println!(
+        "Fewest combined steps to reach crossover: {}",
+        part_two(&input)
+    );
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -63,14 +70,25 @@ fn parse_move(input: &str) -> Move {
 }
 
 fn get_distance_to_nearest_crossover(a: &Route, b: &Route) -> i64 {
-    let places_a_visited = places_visited(&a).collect::<HashSet<(i64, i64)>>();
+    let path_a = places_visited(&a);
+    let path_b = places_visited(&b);
 
-    let places_both_visited = places_visited(b).filter(|place| places_a_visited.contains(place));
-
-    places_both_visited
+    crossover_points(path_a, path_b)
+        .iter()
         .map(|(x, y)| x.abs() + y.abs())
         .min()
         .unwrap()
+}
+
+fn crossover_points<Path>(a: Path, b: Path) -> Vec<Coord>
+where
+    Path: IntoIterator<Item = Coord>,
+{
+    let places_a_visited = a.into_iter().collect::<HashSet<(i64, i64)>>();
+
+    b.into_iter()
+        .filter(|place| places_a_visited.contains(place))
+        .collect::<Vec<Coord>>()
 }
 
 fn places_visited<'a>(route: &'a Route) -> impl Iterator<Item = (i64, i64)> + 'a {
@@ -93,6 +111,44 @@ fn make_move((x, y): &Coord, step: &Dir) -> Coord {
         Dir::Left => (x - 1, *y),
         Dir::Right => (x + 1, *y),
     }
+}
+
+// PART TWO
+
+fn part_two(input: &str) -> i64 {
+    let (a, b) = parse_routes(input);
+
+    fewest_combined_steps_to_crossover(&a, &b)
+}
+
+fn fewest_combined_steps_to_crossover(a: &Route, b: &Route) -> i64 {
+    let path_a = places_visited(&a).collect::<Vec<Coord>>();
+    let path_b = places_visited(&b).collect::<Vec<Coord>>();
+
+    let path_a_clone = path_a.clone();
+    let path_b_clone = path_b.clone();
+
+    let crossovers = crossover_points(path_a, path_b);
+
+    crossovers
+        .iter()
+        .map(|(x, y)| {
+            let steps_a = path_a_clone
+                .iter()
+                .position(|(ax, ay)| ax == x && ay == y)
+                .unwrap()
+                + 1;
+
+            let steps_b = path_b_clone
+                .iter()
+                .position(|(bx, by)| bx == x && by == y)
+                .unwrap()
+                + 1;
+
+            (steps_a + steps_b) as i64
+        })
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -125,5 +181,38 @@ mod part_one_tests {
         );
 
         assert_eq!(get_distance_to_nearest_crossover(&a, &b), 135);
+    }
+}
+
+#[cfg(test)]
+mod part_two_tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let a = parse_route("R8,U5,L5,D3");
+        let b = parse_route("U7,R6,D4,L4");
+
+        assert_eq!(fewest_combined_steps_to_crossover(&a, &b), 30);
+    }
+
+    #[test]
+    fn example_test_case_1() {
+        let (a, b) = parse_routes(
+            "R75,D30,R83,U83,L12,D49,R71,U7,L72
+            U62,R66,U55,R34,D71,R55,D58,R83",
+        );
+
+        assert_eq!(fewest_combined_steps_to_crossover(&a, &b), 610);
+    }
+
+    #[test]
+    fn example_test_case_2() {
+        let (a, b) = parse_routes(
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+            U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+        );
+
+        assert_eq!(fewest_combined_steps_to_crossover(&a, &b), 410);
     }
 }
